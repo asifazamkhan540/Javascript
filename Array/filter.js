76d24b17a91d3f39838e3be4145d09c63bcb4a2c
Asif Azam <your-email@example.com>
2026-08-13 11:42:40 +0500

    update

--- Array/filter.js
+++ Array/filter.js
@@ -2,28 +2,28 @@
 
 let numbers = [2, 5, 7, 8, 12, 15];    
 let numbersResults = numbersResults.filter(function(numbers){
-    return numbersResults > 5;
+    return numbers > 5;
 })
  console.log(numbersResults);
 
 
  let num = [5, 12, 3, 20, 8, 15];
  let numResults = num.filter(function(num){
-    return numResults < 10 ;
+    return num < 10 ;
  })
  console.log(numResults);
 
 
  let names = ["Ali", "Asif", "Ahmed", "sara", "Usaman"];
  let NameUpdates = names.filter(function(names){
-    return namesUdates.length > 4;
+    return names.length > 4;
  })
  console.log(NameUpdates);
 
 
  const arr = [1, 3, 2, 5, 6, 7, 8];
  const ArrResults = arr.filter(function(arr){
-   return ArrResults % 2 === 0;
+   return arr % 2 === 0;
  })
  console.log(ArrResults)
  console.log(arr)
